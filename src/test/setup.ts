9b1ad71812e38e9d61fd17a3fcb9566b3a1543d4
academiaarcana <arcanadevacademia@gmail.com>
2026-08-31 17:25:55 -0400

    fix(test): type React act environment setup correctly

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,7 +1,12 @@
+import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
+
+declare global {
+  var IS_REACT_ACT_ENVIRONMENT: boolean;
+}
 
 globalThis.IS_REACT_ACT_ENVIRONMENT = true;
 
 afterEach(() => {
-  document.body.replaceChildren();
+  cleanup();
 });
