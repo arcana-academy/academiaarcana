8945b128c594efd40f4a262b9a0fb330be390354
academiaarcana <arcanadevacademia@gmail.com>
2026-09-01 13:25:06 -0400

    refactor: expose domain architecture policy

--- src/core/architecture/index.ts
+++ src/core/architecture/index.ts
@@ -1,1 +1,8 @@
 export { CORE_DOMAINS, type CoreDomain } from "./domains";
+export {
+  ARCHITECTURE_LAYERS,
+  DOMAIN_DEPENDENCY_DIRECTION,
+  DOMAIN_POLICIES,
+  type ArchitectureLayer,
+  type DomainPolicy,
+} from "./domain-policy";
