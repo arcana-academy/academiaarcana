1c754491aa6ebf497b17137103cbc345f30c17c6
academiaarcana <arcanadevacademia@gmail.com>
2026-09-09 13:58:24 -0400

    Update import path for useMotionPreference hook

--- src/application/flonts/FlontsProvider.tsx
+++ src/application/flonts/FlontsProvider.tsx
@@ -8,7 +8,7 @@ import {
   useState,
 } from "react";
 
-import { useMotionPreference } from "../../application/accessibility-preferences/hooks/hooks";
+import { useMotionPreference } from "@/application/accessibility-preferences/hooks/hooks";
 
 import type {
   FlontsActions,
