f950e2e210b0286c1006c5044194e6a9da15c172
academiaarcana <arcanadevacademia@gmail.com>
2026-09-11 10:17:13 -0400

    fix: import persisted accessibility preferences in provider test

--- src/application/providers/ApplicationProviders.test.tsx
+++ src/application/providers/ApplicationProviders.test.tsx
@@ -5,6 +5,7 @@ import type { Identity } from "@/core/identity";
 
 import type {
   AuthenticatedAccessibilityPreferencesRepository,
+  PersistedAccessibilityPreferences,
 } from "@/core/accessibility-preferences/contracts";
 
 import { ApplicationProviders } from "./ApplicationProviders";
