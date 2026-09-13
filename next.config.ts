6cbeb88864f5464c10f53563804fe3ceab67f97a
academiaarcana <arcanadevacademia@gmail.com>
2026-09-13 18:09:47 -0400

    fix: initialize Honeybadger Next.js integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+import { setupHoneybadger } from "@honeybadger-io/nextjs";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default setupHoneybadger(nextConfig);
